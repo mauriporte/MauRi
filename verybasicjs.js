a621398d0c3dc4fa1599c14cd9794d4a789dc3f0
mlporte <mlporte@gmail.com>
2015-11-16 17:07:08 +0100

    HTML
    Basic

---
+++ verybasicjs.js
@@ -0,0 +1,114 @@
+/*
+
+var a = 11;
+var b = a + 1;
+
+// string = text (string of characters)
+
+var c = "hello world";
+var d = 11; //Number
+var e = true; // boolean
+var f = 1.1; //float
+
+//String Operators 
+
+var stringA = "hello";
+var stringB = "bye";
+
+// Boolean operators 
+
+var theseStringsEqual = stringA = stringB; // Sets a boolean (false)
+var theseStringsAreNotEqual = stringA != stringB; //Sets a boolean (true)
+
+// Concatenation
+ var stringC = stringA + " " + stringB; // "hello bye"
+ var stringD = "8" + "6"; // Sets "86" because it's a concatenation.
+
+// Conditionals
+
+if(theseStringsEqual) {
+
+	//Do stuff if both strings equal.
+}
+
+// Operator if not (!)
+
+if (!theseStringsEqual) {
+
+	//Do stuff if both strings are not equal.
+}
+
+if(theseStringsEqual) {
+
+	//Do stuff if both strings equal.
+} else if (stringA.length > 100 {
+    // y además si el String es mayor que 100 caracteres.
+	} else {
+
+}
+
+// Conditionals: Switch
+
+var stringX = "hello";
+
+if(stringX == "hello") {}
+else if(stringX == "closed") {}
+	else if(stringX == "open") {}
+
+
+switch(stringX)
+{ 
+case "hello":
+// Do stuff
+break;
+
+case "closed":
+break;
+
+case "open":
+break}
+
+default:
+// Do whatever if no case is matched
+break;
+
+}
+
+var boolean = confirm ("Do you want to proceed?")
+
+
+
+var text = prompt("What's your name?");
+
+/* En base a confirm(), prompt ("Hola dime algo"), pregunta al usuario:
+
+Como se llama
+Que edad tiene
+En base a la edad, muestra un texto que diga si es joven o viejo.
+Y al final muestra una cadena que diga "Me llamo XXX, tengo XXX años"
+
+*/
+
+var name = prompt("Como te llamas?")
+var age = prompt("Que edad tienes")
+
+var agetype = "";
+
+if(age > 24) {
+	agetype = "viejo";
+}
+else if(age < 24) {
+	agetype = "joven"
+}
+
+var phrase = "Hola mi nombre es " + name + ", tengo " + age + " y soy " + agetype;
+
+console.log (phrase);
+
+// Boolean operators
+
+// && => AND
+// || => OR
+
+var age = 20;
+var name
